8287674efa505d6631db6c8376c28b5f4911b935
harshraj1403 <rajharsh1703@gmail.com>
2024-07-25 12:53:45 +0530

    home added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn={firstName:'Harsh'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'> 
+        <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+             subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={10}
+            totalCurrentBalance={10000}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
